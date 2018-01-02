6506744c6f1f286e23ea81f6133de19bc7310ef8
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2018-01-02 14:39:28 +0100

    Fix tests to match updates on webpage, all passing now.

--- word_validate_fails.sh
+++ word_validate_fails.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "NOTE: Grep will return exit status 1 if all tests are passed."
 cat ./test_data/test1.in | ./cmake-build-debug/seq_word_validation_test | grep FAIL
 cat ./test_data/test2.in | ./cmake-build-debug/seq_word_validation_test | grep FAIL
 cat ./test_data/test3.in | ./cmake-build-debug/seq_word_validation_test | grep FAIL
