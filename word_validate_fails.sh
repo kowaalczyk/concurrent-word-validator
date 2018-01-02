455d2607e2e1d7d84ab43afc49120dca993e8cb2
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2018-01-02 14:16:25 +0100

    Add validation runner scripts

---
+++ word_validate_fails.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cat ./test_data/test1.in | ./cmake-build-debug/seq_word_validation_test | grep FAIL
+cat ./test_data/test2.in | ./cmake-build-debug/seq_word_validation_test | grep FAIL
+cat ./test_data/test3.in | ./cmake-build-debug/seq_word_validation_test | grep FAIL
+cat ./test_data/test4.in | ./cmake-build-debug/seq_word_validation_test | grep FAIL
