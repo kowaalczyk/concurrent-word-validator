07ee83e2d64fb952528283b710862fda126c3c9c
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2018-01-20 21:52:56 +0100

    Add logging config

--- src/config.c
+++ src/config.c
@@ -2,9 +2,19 @@
 // Created by kowal on 13.01.18.
 //
 
-#include <assert.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include "config.h"
+
+void log_formatted(const char *fmt, ...) {
+    if(enable_logging) {
+        va_list args;
+        va_start(args, fmt);
+        vprintf(fmt, args);
+        va_end(args);
+        printf("\n");
+    }
+}
 
 void print_comm_summary(const comm_sumary_t *comm_summary) {
     printf("Snt: %zu\nRcd: %zu\nAcc: %zu\n", comm_summary->snt, comm_summary->rcd, comm_summary->acc);
--- src/config.h
+++ src/config.h
@@ -15,24 +15,38 @@
 
 #define NORMAL_MQ_PRIORITY 1
 
+
+#ifdef PW_ENABLE_LOGGING
+static const bool enable_logging = true;
+#else
+static const bool enable_logging = false;
+#endif /* PW_ENABLE_LOGGING */
+
+/// standard logging function used for debug
+extern void log_formatted(const char *fmt, ...);
+
+
 /**
  * Macros for error handling used within the project
  */
-#define INT_FAIL_IF(x) do {if(x) {if(err != NULL) *err = true; return -1;}} while(false)
-#define VOID_FAIL_IF(x) do {if(x) {if(err != NULL) *err = true; return;}} while(false)
-#define PTR_FAIL_IF(x) do {if(x) {if(err != NULL) *err = true; return NULL;}} while(false)
+#define INT_FAIL_IF(x) do {if(x) {if(err != NULL) *err = true; log_formatted("Fail in %s, function: %s", __FILE__, __func__); return -1;}} while(false)
+#define VOID_FAIL_IF(x) do {if(x) {if(err != NULL) *err = true; log_formatted("Fail in %s, function: %s", __FILE__, __func__); return;}} while(false)
+#define PTR_FAIL_IF(x) do {if(x) {if(err != NULL) *err = true; log_formatted("Fail in %s, function: %s", __FILE__, __func__); return NULL;}} while(false)
 #define HANDLE_ERR(err_handler) do {if(err) {(err_handler)();}} while(false)
 #define HANDLE_ERR_EXIT_WITH_MSG(message) do {if(err) {fprintf(stderr, "%s - error number: %d (%s)\n", message, errno, strerror(errno)); exit(-1);}} while(false)
 #define HANDLE_ERR_EXIT_ERRNO_WITH_MSG(message) do {if(err) {fprintf(stderr, "%s - error number: %d (%s)\n", message, errno, strerror(errno)); exit(-errno);}} while(false)
 #define HANDLE_ERR_DECREMENT_BREAK(to_decrement) do {if(err) (to_decrement)--; break;} while(false)
 #define HANDLE_ERR_DECREMENT_CONTINUE(to_decrement) do {if(err) (to_decrement)--; continue;} while(false)
 
+/// struct containing standard communication summary for application within a project
 typedef struct comm_summary{
     size_t snt;
     size_t rcd;
     size_t acc;
 } comm_sumary_t;
 
+/// prints communication summary to stdout
 extern void print_comm_summary(const comm_sumary_t *comm_summary);
+
 
 #endif //PW_VALIDATOR_CONFIG_H
