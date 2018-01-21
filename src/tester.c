feb2f61c3ff5e279a79ff030097e95bc44267476
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2018-01-21 17:38:08 +0100

    Basic, 2-thread tester, TODO: cleanup and add completeness notification (for testers that never receive !)

--- src/tester.c
+++ src/tester.c
@@ -20,6 +20,7 @@ static size_t await_responses = 0;
 static size_t await_forks = 0;
 static pid_t main_pid;
 static mqd_t tester_mq;
+static mqd_t validator_mq;
 static struct sigaction snt_success_action;
 static comm_sumary_t comm_summary = {0, 0, 0};
 
@@ -131,13 +132,50 @@ void process_response(const tester_mq_msg *msg) {
     }
 }
 
-void collect_forks(bool *err) {
+void collect_sender(bool *err) {
     while(await_forks) {
         pid_t tmp_pid = 0;
         int wait_ret = EXIT_SUCCESS;
         tmp_pid = wait(&wait_ret);
         VOID_FAIL_IF(tmp_pid == -1 || wait_ret != EXIT_SUCCESS);
         await_forks--;
+    }
+}
+
+void read_and_send() {
+    char buffer[WORD_LEN_MAX+2]; // + '\n' and '\0'
+    while(fgets(buffer, sizeof(buffer), stdin) != NULL) {
+        assert(buffer[strlen(buffer)-1] == '\n');
+
+        buffer[strlen(buffer)-1] = '\0';
+        bool start = (buffer[0]!='!');
+        bool halt = !start;
+
+        log_formatted("%d SND: %s", getpid(), buffer);
+        validator_mq_send(validator_mq, start, halt, false, false, main_pid, buffer, &err);
+        HANDLE_ERR(err_notify_parent);
+
+        buffer[0] = '\0'; // following shorter words cannot contain junk
+    }
+}
+
+pid_t async_spawn_sender() {
+    pid_t sender_pid;
+    switch (sender_pid = fork()) {
+        case -1:
+            exit(EXIT_FAILURE); // TODO: Handle error
+        case 0:
+            validator_mq = validator_mq_start(false, &err); // assuming there is only one validator
+            HANDLE_ERR_EXIT_WITH_MSG("TESTER: Failed to start validator mq"); // TODO: Sig parent
+
+            read_and_send();
+
+            validator_mq_finish(false, validator_mq, &err);
+            HANDLE_ERR(err_kill_children_exit); // TODO: Sig parent
+            exit(EXIT_SUCCESS);
+        default:
+            await_forks++;
+            return sender_pid;
     }
 }
 
@@ -153,41 +191,44 @@ int main() {
     tester_mq_get_name_from_pid(main_pid, tester_mq_name);
     tester_mq = tester_mq_start(true, tester_mq_name, &err);
     HANDLE_ERR_EXIT_WITH_MSG("TESTER: Failed to start tester mq");
-    mqd_t validator_mq = validator_mq_start(false, &err); // assuming there is only one validator
-    HANDLE_ERR_EXIT_WITH_MSG("TESTER: Failed to start validator mq");
+//    mqd_t validator_mq = validator_mq_start(false, &err); // assuming there is only one validator
+//    HANDLE_ERR_EXIT_WITH_MSG("TESTER: Failed to start validator mq");
+//
+//    // load words and send them asynchronously
+//    char buffer[WORD_LEN_MAX+2]; // + '\n' and '\0'
+//    while(fgets(buffer, sizeof(buffer), stdin) != NULL) {
+//        assert(buffer[strlen(buffer)-1] == '\n');
+//
+//        buffer[strlen(buffer)-1] = '\0';
+//        log_formatted("%d SND: %s", getpid(), buffer);
+//        async_send_request_to_validator(validator_mq, buffer);
+//        buffer[0] = '\0'; // following shorter words cannot contain junk
+//    }
+//    validator_mq_finish(false, validator_mq, &err);
+//    HANDLE_ERR(err_kill_children_exit);
 
-    // load words and send them asynchronously
-    char buffer[WORD_LEN_MAX+2]; // + '\n' and '\0'
-    while(fgets(buffer, sizeof(buffer), stdin) != NULL) {
-        assert(buffer[strlen(buffer)-1] == '\n');
-
-        buffer[strlen(buffer)-1] = '\0';
-        log_formatted("%d sending: %s", getpid(), buffer);
-        async_send_request_to_validator(validator_mq, buffer);
-        buffer[0] = '\0'; // following shorter words cannot contain junk
-    }
-    validator_mq_finish(false, validator_mq, &err);
-    HANDLE_ERR(err_kill_children_exit);
+    pid_t sender_pid = async_spawn_sender(); // TODO: Global
 
     // process validator responses
     tester_mq_msg tester_msg;
-    while(await_responses) {
+    while(true) {
         tester_mq_receive(tester_mq, &tester_msg, &err);
         HANDLE_ERR(err_kill_children_exit);
 
         await_responses--;
-        log_formatted("%d received response for: %s", getpid(), tester_msg.word);
         process_response(&tester_msg);
         if(tester_msg.completed) {
+            kill(sender_pid, 9); // TODO: change this to something normal
+            // completed is received only once, and no messages will be sent from server after that
             break;
         }
     }
     tester_mq_finish(true, tester_mq, tester_mq_name, &err);
     HANDLE_ERR(err_kill_children_exit);
 
-    // collect forks and finish
-    collect_forks(&err);
-    HANDLE_ERR_EXIT_WITH_MSG("TESTER: Unexpected fail while collecting forks");
+    // clean up
+    collect_sender(&err);
+    HANDLE_ERR_EXIT_WITH_MSG("TESTER: Unexpected fail while collecting message sender");
     print_comm_summary(&comm_summary);
     return 0;
 }
