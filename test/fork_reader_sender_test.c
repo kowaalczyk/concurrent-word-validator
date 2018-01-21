84096786584a81731b2947422b71fa7b9f08c3e1
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2018-01-21 17:08:00 +0100

    Add test w/ outline of dual tester, passing w/ pipes and redirects

---
+++ test/fork_reader_sender_test.c
@@ -0,0 +1,31 @@
+//
+// Created by kowal on 21.01.18.
+//
+
+#include <zconf.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <memory.h>
+#include <wait.h>
+
+int main() {
+    char buffer[1000];
+
+    switch (fork()) {
+        case -1:
+            exit(EXIT_FAILURE);
+        case 0:
+            while(fgets(buffer, sizeof(buffer), stdin) != NULL) {
+                assert(buffer[strlen(buffer)-1] == '\n');
+
+                buffer[strlen(buffer)-1] = '\0';
+                printf("%d SND: %s\n", getpid(), buffer);
+                buffer[0] = '\0'; // following shorter words cannot contain junk
+            }
+        default:
+            close(0);
+            wait(NULL);
+    }
+    return 0;
+}
