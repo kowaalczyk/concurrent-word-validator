6e9366e46e958040c01244ac99d4342b54f99bc6
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2018-01-21 21:13:55 +0100

    Updated error macros

--- src/config.h
+++ src/config.h
@@ -26,18 +26,19 @@ static const bool enable_logging = false;
 extern void log_formatted(const char *fmt, ...);
 
 
-/**
- * Macros for error handling used within the project
- */
+/// Macros for error throwing
 #define INT_FAIL_IF(x) do {if(x) {if(err != NULL) *err = true; log_formatted("Fail in %s, function: %s", __FILE__, __func__); return -1;}} while(false)
 #define VOID_FAIL_IF(x) do {if(x) {if(err != NULL) *err = true; log_formatted("Fail in %s, function: %s", __FILE__, __func__); return;}} while(false)
 #define PTR_FAIL_IF(x) do {if(x) {if(err != NULL) *err = true; log_formatted("Fail in %s, function: %s", __FILE__, __func__); return NULL;}} while(false)
+
+/// Helper for printing errors
+#define INNER_PRINT_MSG__(message) do {fprintf(stderr, "%s - error number: %d (%s)\n", message, errno, strerror(errno));} while(false);
+
+/// Macros for error handling
 #define HANDLE_ERR(err_handler) do {if(err) {(err_handler)();}} while(false)
-#define HANDLE_ERR_PRINT_MSG(message) do {if(err) {fprintf(stderr, "%s - error number: %d (%s)\n", message, errno, strerror(errno));}} while(false)
-#define HANDLE_ERR_WITH_MSG(err_handler, message) do {if(err) {fprintf(stderr, "%s - error number: %d (%s)\n", message, errno, strerror(errno)); (err_handler)();}} while(false)
-#define HANDLE_ERR_EXIT_WITH_MSG(message) do {if(err) {fprintf(stderr, "%s - error number: %d (%s)\n", message, errno, strerror(errno)); exit(EXIT_FAILURE);}} while(false)
-#define HANDLE_ERR_DECREMENT_BREAK(to_decrement) do {if(err) (to_decrement)--; break;} while(false)
-#define HANDLE_ERR_DECREMENT_CONTINUE(to_decrement) do {if(err) (to_decrement)--; continue;} while(false)
+#define HANDLE_ERR_PRINT_MSG(message) do {if(err) {INNER_PRINT_MSG__(message);}} while(false)
+#define HANDLE_ERR_WITH_MSG(err_handler, message) do {if(err) {INNER_PRINT_MSG__(message); (err_handler)();}} while(false)
+#define HANDLE_ERR_EXIT_WITH_MSG(message) do {if(err) {INNER_PRINT_MSG__(message); exit(EXIT_FAILURE);}} while(false)
 
 /// struct containing standard communication summary for application within a project
 typedef struct comm_summary{
